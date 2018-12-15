073c5bd4c36cb06eb7097ce2e0f7cb83305c8a59
Tolulope Odueke <tolulope.od@gmail.com>
2018-12-15 15:58:26 +0100

    Created Navbar and Landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello There!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Landing />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Boilerplate </b> login/authentication app with the{" "}
+              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
+              scratch
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              Creare a (minimal) full-stack app with user authentication via
+              passport and JSON Web Tokens
+            </p>
+            <br />
+            <a
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Register
+            </a>
+            <a
+              style={{
+                marginLeft: "2rem",
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect white hoverable black-text"
+            >
+              Log In
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
